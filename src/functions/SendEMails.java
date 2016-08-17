f8803b73edc9f34870f9d1c9c3f3fe235d80ef3e
KGRS <KGRS@192.168.1.2>
2016-08-17 17:15:37 +0530

    pro management commit 2016-8-17 by Ravindu

--- src/functions/SendEMails.java
+++ src/functions/SendEMails.java
@@ -30,56 +30,56 @@ import reports.JobMoniter;
  */
 public class SendEMails {
 
-    public void registrationEmail(String studentID) {
-        final String username = "ravindu.it10005040@gmail.com";
-        final String password = "ravIT10005040";
-        Properties props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-        try {
-            ResultSet reset;
-            Statement stmt;
-            String query;
-            query = "SELECT EMAIL, STUDENT_FIRST_NAME, STUDENT_SUR_NAME FROM students WHERE STUDENT_ID = '" + studentID + "'";
-            stmt = ConnectSql.conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            reset = stmt.executeQuery(query);
-
-            if (reset.next()) {
-                String senderEmail = reset.getString("EMAIL");
-                String firstName = reset.getString("STUDENT_FIRST_NAME");
-                String surName = reset.getString("STUDENT_SUR_NAME");
-
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress("ravindu.it10005040@gmail.com"));
-                message.setRecipients(Message.RecipientType.TO,
-                        InternetAddress.parse(senderEmail)); //ravindu.spsolutions@gmail.com
-                message.setSubject("Registration confirmation of EDU institute.");
-                message.setText("Dear '" + firstName + "''" + " " + "''" + surName + "',"
-                        + "\n\nCongradulations. Your request of register to EDU institute is approved."
-                        + "\nYour registration id is '" + studentID + "'."
-                        + "\n"
-                        + "\nThank you."
-                        + "\nEDU team.");
-
-                Transport.send(message);
-                System.out.println("Done");
-                JOptionPane.showMessageDialog(null, "Registration email is sent to '" + studentID + "'.");
-            }
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException ex) {
-            Logger.getLogger(SendEMails.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+//    public void registrationEmail(String studentID) {
+//        final String username = "ravindu.it10005040@gmail.com";
+//        final String password = "ravIT10005040";
+//        Properties props = new Properties();
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.host", "smtp.gmail.com");
+//        props.put("mail.smtp.port", "587");
+//        Session session = Session.getInstance(props,
+//                new javax.mail.Authenticator() {
+//                    @Override
+//                    protected PasswordAuthentication getPasswordAuthentication() {
+//                        return new PasswordAuthentication(username, password);
+//                    }
+//                });
+//        try {
+//            ResultSet reset;
+//            Statement stmt;
+//            String query;
+//            query = "SELECT EMAIL, STUDENT_FIRST_NAME, STUDENT_SUR_NAME FROM students WHERE STUDENT_ID = '" + studentID + "'";
+//            stmt = ConnectSql.conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//            reset = stmt.executeQuery(query);
+//
+//            if (reset.next()) {
+//                String senderEmail = reset.getString("EMAIL");
+//                String firstName = reset.getString("STUDENT_FIRST_NAME");
+//                String surName = reset.getString("STUDENT_SUR_NAME");
+//
+//                Message message = new MimeMessage(session);
+//                message.setFrom(new InternetAddress("ravindu.it10005040@gmail.com"));
+//                message.setRecipients(Message.RecipientType.TO,
+//                        InternetAddress.parse(senderEmail)); //ravindu.spsolutions@gmail.com
+//                message.setSubject("Registration confirmation of EDU institute.");
+//                message.setText("Dear '" + firstName + "''" + " " + "''" + surName + "',"
+//                        + "\n\nCongradulations. Your request of register to EDU institute is approved."
+//                        + "\nYour registration id is '" + studentID + "'."
+//                        + "\n"
+//                        + "\nThank you."
+//                        + "\nEDU team.");
+//
+//                Transport.send(message);
+//                System.out.println("Done");
+//                JOptionPane.showMessageDialog(null, "Registration email is sent to '" + studentID + "'.");
+//            }
+//        } catch (MessagingException e) {
+//            throw new RuntimeException(e);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(SendEMails.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 
     public static String notifyAboutLateJobsToSupervisourByEmail(String SUPERVISE_BY, String JOB_ID, String JOB_ALLOCATED_DATE, String JOB_ALLOCATED_TIME, int ALLOCATED_TIME, String PRODUCT_LEVEL_ITEM_CODE, String SHOULD_FINISHED_DATE, String SHOULD_FINISHED_AT) {
         final String BUSINESS_NAME, space = " ";
